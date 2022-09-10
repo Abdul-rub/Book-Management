632f25d96a0e6a97342525c695d6e8c756f351d8
Abdul-rub <arub0419@gmail.com>
2022-09-11 02:15:14 +0530

    login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Login = () => {
+   const [email,setEmail] = useState('')
+   const [password,setPassword]=useState('')
+
+   const handleSubmit=()=>{
+    
+   }
+
   return (
-    <div>Login</div>
+    <div>
+      <form onSubmit={handleSubmit} >
+        <div>
+          <label htmlFor="">UserEmail</label>
+          <input type="email" />
+        </div>
+        <div>
+          <label htmlFor="">User Password</label>
+          <input type="password" />
+        </div>
+        <button type='submit'>Login</button>
+      </form>
+    </div>
   )
 }
