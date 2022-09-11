1bf15758753f8bb42d8020c30fb286c7d70c79b1
Abdul-rub <arub0419@gmail.com>
2022-09-11 11:46:10 +0530

    ..

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,19 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { login } from '../Redux/Auth/action'
 
 export const Login = () => {
    const [email,setEmail] = useState('')
    const [password,setPassword]=useState('')
+   const dispatch = useDispatch();
 
-   const handleSubmit=()=>{
-    
+   const handleSubmit=(e)=>{
+      e.preventDefault();
+      if(email && password){
+        dispatch(login({email,password})).then((res)=>{
+          console.log('After Successful login',res)
+        })
+      }
    }
 
   return (
@@ -13,11 +21,17 @@ export const Login = () => {
       <form onSubmit={handleSubmit} >
         <div>
           <label htmlFor="">UserEmail</label>
-          <input type="email" />
+          <input type="email" 
+          value={email}
+          onChange={(e)=>setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="">User Password</label>
-          <input type="password" />
+          <input type="password"
+          value={password}
+          onChange={(e)=>setPassword (e.target.value)}
+          />
         </div>
         <button type='submit'>Login</button>
       </form>
--- src/Redux/Auth/action.js
+++ src/Redux/Auth/action.js
@@ -2,10 +2,10 @@ import * as types from "./actionTypes"
 import axios from "axios"
 
 
-export const login =()=>(dispatch)=>{
+export const login =(payload)=>(dispatch)=>{
     dispatch({type:types.USER_LOGIN_REQUEST})
     return axios.post(`https://reqres.in/api/login`,payload)
-    .then((res)=>{dispatch({type:types.USER_LOGIN_SUCCESS,payload:res.data.token})})
+    .then((res)=>{return dispatch({type:types.USER_LOGIN_SUCCESS,payload:res.data.token})})
     .catch((err)=>{dispatch({type:types.USER_LOGIN_FAILURE,payload:err})})
 
 }
